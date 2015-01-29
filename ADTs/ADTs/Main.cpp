56bce24898aae756f97652f8751d2a9b3463ec69
cpi-cecko <gogo_rus@abv.bg>
2015-01-29 14:56:05 +0200

    adds some comments

--- ADTs/ADTs/Main.cpp
+++ ADTs/ADTs/Main.cpp
@@ -1,3 +1,18 @@
+// C++ equivalent of the following Haskell code:
+// data Shape = Circle Float
+//			  | Rect Float Float
+//			  | Square Float
+//
+// area :: Shape -> Float
+// area (Circle r) = pi * r * r
+// area (Rect d h) = d * h
+// area (Square d) = d * d
+//
+// circ :: Shape -> Float
+// circ (Circle r) = 2.0 * pi * r
+// circ (Rect d h) = 2.0 * (d + h)
+// circ (Square d) = 4.0 * d
+//
 #include <iostream>
 
 
